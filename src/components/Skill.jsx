836ddce757d19ed98521f31f1e749254b7c63e29
Biswajit Roy <biswajitr581@gmail.com>
2025-10-15 05:47:29 +0530

    new skill added

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -31,6 +31,21 @@ function Skill() {
       id: 6,
       imgsrc: "/Langues/bootstrap.png",
       text: "Bootstrap",
+    },
+    {
+      id: 7,
+      imgsrc: "/Langues/git.jpeg",
+      text: "git",
+    },
+    {
+      id: 8,
+      imgsrc: "/Langues/github.png",
+      text: "GitHub",
+    },
+    {
+      id: 9,
+      imgsrc: "/Langues/terminal.png",
+      text: "Terminal",
     }
   ]
   return (
@@ -38,9 +53,9 @@ function Skill() {
       <div className='max-w-screen-2xl container mx-auto px-4 md:px-20 mt-6' name="Skills">
         <h1 className='md:text-3xl text-xl font-bold md:mb-5 mb-4'>Skills</h1>
         <p className='text-md md:text-xl'>I'm Skilled to work with below technologies:</p>
-        <div className='flex-col flex md:flex-row md:justify-between items-center w-full my-4'>
+        <div className='flex-col flex md:flex-row flex-wrap  items-center w-full my-4'>
           {skillItems.map(({ id, imgsrc, text }) =>
-            <div key={id} className='h-[150px] w-[150px] md:h-[200px] md:w-[200px] rounded-full border-[1px] flex justify-center items-center m-1.5 hover:scale-105 duration-200'>
+            <div key={id} className='h-[150px] w-[150px] md:h-[200px] md:w-[200px] rounded-full border-[1px]  flex justify-center items-center m-1.5 hover:scale-105 duration-200'>
               <div className='flex flex-col items-center justify-center'>
                 <img src={imgsrc} alt="" className='h-[60px] w-[60px] md:h-[80px] md:w-[80px] rounded-full' />
                 <p className='text-center w-full'>{text}</p>
